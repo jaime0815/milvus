2ccf1476237bdbf5e6bf0b5d785bb64850d9919c
yun.zhang <yun.zhang@zilliz.com>
2022-01-11 21:58:31 +0800

    Support llvm13
    Signed-off-by: yun.zhang <yun.zhang@zilliz.com>

--- scripts/core_build.sh
+++ scripts/core_build.sh
@@ -149,12 +149,13 @@ unameOut="$(uname -s)"
 case "${unameOut}" in
     Darwin*)
         llvm_prefix="$(brew --prefix llvm)"
+        export CLANG_TOOLS_PATH="${llvm_prefix}/bin"
         export CC="${llvm_prefix}/bin/clang"
         export CXX="${llvm_prefix}/bin/clang++"
         export LDFLAGS="-L${llvm_prefix}/lib -L/usr/local/opt/libomp/lib"
         export CXXFLAGS="-I${llvm_prefix}/include -I/usr/local/include -I/usr/local/opt/libomp/include -Wno-error=deprecated-declarations -Wno-deprecated-declarations -DBOOST_STACKTRACE_GNU_SOURCE_NOT_REQUIRED=1"
         ;;
-          *)   echo "UNKNOWN:${unameOut}"; exit 0;
+          *)   echo "==System:${unameOut}";
 esac
 
 
